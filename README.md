d4b5108ebe7304ce1b7b8df9746d8ff9ac79b90f
Ludovic Tolhurst-Cleaver <camel@ltcdev.com>
2022-05-24 13:48:52 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,23 @@
+# NAME
+
+Cart::Kata - Shopping Cart Exercise
+
+# SYNOPSIS
+
+    use Cart::Kata;
+
+# DESCRIPTION
+
+Cart::Kata offers an implemented solution to the 'cart_kata' Shopping Cart challenge set by SpareRoom, a.k.a. Flatshare Ltd., as part of their recruitment process. 
+
+
+# LICENSE
+
+Copyright (C) Ludovic Tolhurst-Cleaver.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+# AUTHOR
+
+Ludovic Tolhurst-Cleaver <camel@ltcdev.com>
---
+++ lib/Cart/Kata.pm
@@ -0,0 +1,39 @@
+package Cart::Kata;
+use 5.008001;
+use strict;
+use warnings;
+
+our $VERSION = "0.01";
+
+
+
+1;
+__END__
+
+=encoding utf-8
+
+=head1 NAME
+
+Cart::Kata - It's new $module
+
+=head1 SYNOPSIS
+
+    use Cart::Kata;
+
+=head1 DESCRIPTION
+
+Cart::Kata is ...
+
+=head1 LICENSE
+
+Copyright (C) Ludovic Tolhurst-Cleaver.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=head1 AUTHOR
+
+Ludovic Tolhurst-Cleaver E<lt>camel@ltcdev.comE<gt>
+
+=cut
+
